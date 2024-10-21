5f45f08178892382e4c6091ba055264aa12014af
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-10-21 13:31:12 +0530

    Wrap user SignIn

--- controller/auth.js
+++ controller/auth.js
@@ -1,11 +1,20 @@
+const userModel = require("../model/user");
 const userSchemaZod = require("../utils/validation")
 
-function handleUserSignUp(req, res) {
-    const validatatedData = userSchemaZod.parse(req.body);
-    if (!validatatedData) {
-        console.log("issue in data");
-    } else {
-        console.log("dATA:", validatatedData);
+async function handleUserSignUp(req, res) {
+    try {
+        const validatatedData = userSchemaZod.parse(req.body);
+        if (validatatedData) {
+            const user = new userModel(validatatedData);
+            const savedUser = await user.save();
+            res.status(200).json(
+                savedUser
+            )
+        }
+    } catch (error) {
+        res.status(400).json({
+            message: `Something went wrong! ${error}`
+        })
     }
 }
 
