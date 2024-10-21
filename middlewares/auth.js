e3f6c7e057535c467619dc1af71d28bd315fb0aa
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-10-21 16:37:59 +0530

    Implement UserAuth Middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+    try {
+        const { token } = req.cookies;
+        if (!token) {
+            return res.status(401).send("Please Login!");
+        }
+
+        const decodedObj = jwt.verify(token, process.env.JWT_SECRET);
+
+        const { _id } = decodedObj;
+
+        const user = await userModel.findById(_id);
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        res.status(400).send("Opps Something went wrong!: " + err.message);
+    }
+};
+
+module.exports = {
+    userAuth,
+};
