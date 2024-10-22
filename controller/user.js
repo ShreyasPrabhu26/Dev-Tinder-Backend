95d2a8194919586a717020c64e6078a2b92cf937
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-10-22 15:30:55 +0530

    Implement Get Connections

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,4 @@
 const ConnectionRequestModel = require("../model/connectionRequest");
-const { aggregate } = require("../model/user");
 
 async function handleGetRecivedRequest(req, res) {
     try {
@@ -22,6 +21,38 @@ async function handleGetRecivedRequest(req, res) {
     }
 }
 
+async function handleGetConnections(req, res) {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequestModel.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+            ],
+        })
+            .populate("fromUserId", ["firstName", "lastName", "photoUrl", "gender", "about", "skills"])
+            .populate("toUserId", ["firstName", "lastName", "photoUrl", "gender", "about", "skills"]);
+
+        if (!connectionRequests || connectionRequests.length === 0) {
+            return res.status(404).send("No connections found.");
+        }
+
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.status(200).json({ data });
+
+    } catch (error) {
+        res.status(400).send("Oops! Something went wrong: " + error.message);
+    }
+}
+
 module.exports = {
-    handleGetRecivedRequest
+    handleGetRecivedRequest,
+    handleGetConnections
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
-const { handleGetRecivedRequest } = require("../controller/user");
+const { handleGetRecivedRequest, handleGetConnections } = require("../controller/user");
 
 const userRoute = express.Router();
 
 userRoute.get("/requests/recived", userAuth, handleGetRecivedRequest)
+userRoute.get("/connections", userAuth, handleGetConnections)
 
 module.exports = userRoute;
