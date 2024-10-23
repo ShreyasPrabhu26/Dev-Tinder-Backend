76b5c17933c86a37e970ae75b1cf38f420e73b1e
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-10-23 12:46:23 +0530

    Refactor Auth Middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,22 +5,30 @@ const userAuth = async (req, res, next) => {
     try {
         const { token } = req.cookies;
         if (!token) {
-            return res.status(401).send("Please Login!");
+            return res.status(401).send("Please log in!");
         }
 
+        // Verify the token
         const decodedObj = jwt.verify(token, process.env.JWT_SECRET);
-
         const { _id } = decodedObj;
 
+        // Fetch the user from the database
         const user = await userModel.findById(_id);
         if (!user) {
-            throw new Error("User not found");
+            return res.status(404).send("User not found");
         }
 
         req.user = user;
         next();
     } catch (err) {
-        res.status(400).send("Opps Something went wrong!: " + err.message);
+        if (err.name === "JsonWebTokenError") {
+            return res.status(401).send("Invalid token");
+        }
+        if (err.name === "TokenExpiredError") {
+            return res.status(401).send("Token expired, please log in again");
+        }
+
+        res.status(400).send("Oops, something went wrong! " + err.message);
     }
 };
 
