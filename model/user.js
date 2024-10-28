2b68fa96b3744649dd82513fe9540dabd8edde2b
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-10-28 14:48:55 +0530

    Implement ondelete cascade

--- model/user.js
+++ model/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { defaultUserProfilePhoto } = require("../utils/constants");
+const ConnectionRequestModel = require("./connectionRequest");
 
 const userSchema = new mongoose.Schema(
     {
@@ -46,35 +47,60 @@ const userSchema = new mongoose.Schema(
         skills: {
             type: [String],
         },
+        deleted: {
+            type: Boolean,
+            default: false
+        },
     },
     {
         timestamps: true,
     }
 );
 
+// Hash the password before saving
+userSchema.pre("save", async function (next) {
+    const user = this;
+
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 10);
+    }
+    next();
+});
+
+// Cascade delete connection requests when user is deleted
+userSchema.pre("remove", async function (next) {
+    const user = this;
+    try {
+        await ConnectionRequestModel.deleteMany({
+            $or: [
+                { fromUserId: user._id },
+                { toUserId: user._id }
+            ]
+        });
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Generate JWT token
 userSchema.methods.getJWT = function () {
     const user = this;
 
-    const token = jwt.sign({ _id: user._id },
-        process.env.JWT_SECRET, {
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
         expiresIn: "2d",
     });
 
     return token;
 };
 
+// Validate password
 userSchema.methods.validatePassword = async function (inputPassword) {
     const user = this;
-    const passwordHash = user.password;
-
-    const isPasswordValid = await bcrypt.compare(
-        inputPassword,
-        passwordHash
-    );
-
+    const isPasswordValid = await bcrypt.compare(inputPassword, user.password);
     return isPasswordValid;
-}
+};
 
-const userModel = mongoose.model("User", userSchema);
+const UserModel = mongoose.model("User", userSchema);
 
-module.exports = userModel;
+module.exports = UserModel;
